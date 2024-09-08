4d016d5b118b428bbf923595fb5fa0d16f7d398a
Oliulla <razasm29@gmail.com>
2024-09-08 23:34:24 +0600

    delete metadata code from layout

--- src/app/(pages)/layout.js
+++ src/app/(pages)/layout.js
@@ -8,58 +8,6 @@ import Footer from "@/components/layout/Footer/Footer";
 
 const roboto = Roboto_Slab({ subsets: ['latin'], display: 'swap', adjustFontFallback: false });
 
-export async function generateMetadata() {
-  try {
-    const [metaDataResponse, googleVerificationResponse] = await Promise.all([
-      fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/home`, { cache: "no-store" }),
-      fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/siteMap`, { cache: "no-store" }),
-    ]);
-
-    if (!metaDataResponse.ok || !googleVerificationResponse.ok) {
-      console.error('Network response was not ok');
-    }
-
-    const metaData = await metaDataResponse.json();
-    const googleVerification = await googleVerificationResponse.json();
-
-    const googleConsoleKey = extractGoogleConsoleKey(googleVerification);
-
-    const {
-      title = "Construction Company in Brooklyn | RH Construction USA Inc.",
-      description = "As one of the leading construction companies in Brooklyn, RH Construction USA Inc. offers top-notch general contractor services in Brooklyn.",
-      keywords = "Home, RH Construction USA"
-    } = metaData?.homeRouteAllMetaData?.[0] || {};
-
-    return {
-      title,
-      description,
-      keywords,
-      verification: {
-        google: googleConsoleKey,
-      }
-    };
-  } catch (error) {
-    return {
-      title: "Construction Company in Brooklyn | RH Construction USA Inc.",
-      description: "As one of the leading construction companies in Brooklyn, RH Construction USA Inc. offers top-notch general contractor services in Brooklyn.",
-      keywords: "Home, RH Construction USA",
-    };
-  }
-}
-
-function extractGoogleConsoleKey(googleVerification) {
-  try {
-    const { sitemap } = googleVerification || {};
-    if (!sitemap || !sitemap[0]?.title) return "";
-    const metaTagContent = sitemap[0].title;
-    const consoleKey = metaTagContent.split("=").pop().slice(1, -4);
-    return consoleKey;
-  } catch (error) {
-    console.error('Error extracting Google console key:', error);
-    return "";
-  }
-}
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
@@ -71,7 +19,7 @@ export default function RootLayout({ children }) {
         <main className="min-h-[60vh]">
           {children}
         </main>
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
