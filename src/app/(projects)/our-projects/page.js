1d0a03b965d71e8431d731d0b6862075df6c697e
Oliulla <razasm29@gmail.com>
2024-09-08 23:37:03 +0600

    remove meta generate code

--- src/app/(projects)/our-projects/page.js
+++ src/app/(projects)/our-projects/page.js
@@ -1,37 +1,6 @@
 import ProjectsComponent from "@/components/Projects/ProjectComponents/ProjectComponent";
 
-export async function generateMetadata() {
-  try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/projects`);
 
-    if (!response.ok) {
-      console.log('Network response was not ok.');
-    }
-
-    const data = await response.json();
-    const metaData = data?.metaData?.[0] || {};
-    const {
-      title = 'Best General Contractor in Brooklyn | RH Construction USA Inc.',
-      description = `Best general contractor in Brooklyn, specializing in home renovations, bathroom 
-      renovations & kitchen remodeling, roofing, and construction services.`,
-      keywords = `General Contractor, General Contractor in Brooklyn, 
-      RH Construction USA Inc, home renovations, bathroom & kitchen remodeling, roofing` }
-      = metaData;
-
-    return {
-      title,
-      description,
-      keywords,
-    };
-  } catch (error) {
-    console.error('Error fetching metadata:', error.message);
-    return {
-      title: 'Best General Contractor in Brooklyn | RH Construction USA Inc.',
-      description: 'Best general contractor in Brooklyn, specializing in home renovations, bathroom renovations & kitchen remodeling, roofing, and construction services.',
-      keywords: 'General Contractor, General Contractor in Brooklyn, RH Construction USA Inc, home renovations, bathroom & kitchen remodeling, roofing',
-    };
-  }
-}
 
 export default async function Page() {
   const res = await getData();
