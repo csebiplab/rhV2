fbc57d455286e4175a658b3da23e9b4bb86de4d2
Oliulla <razasm29@gmail.com>
2024-09-14 21:43:47 +0600

    deleted old dashboard

--- src/config/API.config.js
+++
@@ -1,6 +0,0 @@
-const { default: axios } = require("axios");
-
-const API = axios.create({
-  baseURL: "http://localhost:3000/api/",
-});
-export default API;
--- src/config/api.endpoints.js
+++
@@ -1,8 +0,0 @@
-const endPoints = {
-    metadata: "/metadata",
-    googleAnalytics: "/search-console",
-    robotTxt: "/robotTxt",
-    siteMap: "/siteMap",
-};
-
-export const { metadata, googleAnalytics, robotTxt, siteMap } = endPoints;
